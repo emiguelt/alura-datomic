4f27f9da96d5efeff6befabc5be7b72ebb498c9b
Miguel Triana <edwintriana@gmail.com>
2022-01-20 18:34:13 -0500

    using an instant of time to query data

--- src/ecommerce_filtros/db/venda.clj
+++ src/ecommerce_filtros/db/venda.clj
@@ -16,3 +16,21 @@
                        :venda/id id
                        }])
     id))
+
+(defn instante-da-venda [db venda-id]
+  (d/q '[:find ?instante .
+         :in $ ?id
+         :where [_ :venda/id ?id ?tx true]
+         [?tx :db/txInstant ?instante]] db venda-id))
+
+;; calcula o custo da venda no instante que foi creada
+(defn custo [db venda-id]
+  (let [instante (instante-da-venda db venda-id)]
+    (d/q '[:find (sum ?preco-por-quantidade) .
+           :in $ in
+           :where [?venda :venda/id ?id]
+           [?venda :venda/quantidade ?quantidade]
+           [?venda :venda/produto ?produto]
+           [?produto :produto/preco ?preco]
+           [(* ?preco ?quantidade) ?preco-por-quantidade]]
+      (d/as-of db instante) venda-id)))
